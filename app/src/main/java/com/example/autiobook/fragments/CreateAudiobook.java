e869fba5a164747dc4f80b808c9a30f028ecf139
rquit <nabilomi1@gmail.com>
2022-05-06 23:59:27 -0400

    add file upload

--- app/src/main/java/com/example/autiobook/fragments/CreateAudiobook.java
+++ app/src/main/java/com/example/autiobook/fragments/CreateAudiobook.java
@@ -1,5 +1,9 @@
 package com.example.autiobook;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -13,39 +17,33 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Objects;
+
 
 public class CreateAudiobook extends Fragment {
+
+    static final int PICK_TEXT_FILE = 8000;
+
+    Uri chosenFile = null;
 
     EditText tvTitle;
     Button btnUpload;
     TextView tvPreview;
     Button btnCreate;
-
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    String book;
 
     public CreateAudiobook() {
         // Required empty public constructor
     }
 
 
-    public static CreateAudiobook newInstance(String param1, String param2) {
-        CreateAudiobook fragment = new CreateAudiobook();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            // TODO: Rename and change types of parameters
-            String mParam1 = getArguments().getString(ARG_PARAM1);
-            String mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -63,5 +61,55 @@ public class CreateAudiobook extends Fragment {
         btnUpload = requireView().findViewById(R.id.btnUpload);
         tvPreview = requireView().findViewById(R.id.tvPreview);
         btnCreate = requireView().findViewById(R.id.btnCreate);
+
+        btnUpload.setOnClickListener(e -> openFile());
     }
+
+    // I do not care that this is deprecated this way is easier than the new one.
+    @SuppressWarnings("deprecation")
+    @SuppressLint("SetTextI18n")
+    private void openFile() {
+        tvPreview.setText("Loading...");
+        Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        i.addCategory(Intent.CATEGORY_OPENABLE);
+        i.setType("text/plain");
+
+        startActivityForResult(i, PICK_TEXT_FILE);
+    }
+
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_TEXT_FILE && resultCode == Activity.RESULT_OK) {
+            if(data != null) {
+                chosenFile = data.getData();
+            }
+
+            try {
+                if(chosenFile != null) {
+                    book = readTextFromUri(chosenFile);
+                    tvPreview.setText(book);
+                }
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+        }
+    }
+
+    private String readTextFromUri(Uri uri) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (InputStream inputStream =
+                     requireContext().getContentResolver().openInputStream(uri);
+             BufferedReader reader = new BufferedReader(
+                     new InputStreamReader(Objects.requireNonNull(inputStream)))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                line = line + "\n";
+                stringBuilder.append(line);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
 }
